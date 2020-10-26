874d26ae41cae1ca25b2b8f13321bf69f768ae23
mzimic20 <mzimic20@stuy.edu>
2020-10-26 17:15:50 -0400

    Step 1 complete

---
+++ BankAccount.java
@@ -0,0 +1,13 @@
+public class BankAccount {
+
+  private double balance;
+  private int accountID;
+  private String password;
+
+  public BankAccount(int acc; String pass) {
+    balance = 0.0;
+    accountID = acc;
+    password = pass;
+  }
+
+}
